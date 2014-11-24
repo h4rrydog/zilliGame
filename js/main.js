82e4d312b57623ce84786fbda8825d795d39a79a
h4rrydog <ifung@guanxin.de>
2014-11-24 15:17:27 +0000

    Added all carousel assets. Wired up slick to carousel.

--- js/main.js
+++ js/main.js
@@ -3,3 +3,49 @@ $('.img-holder').imageScroll({
     coverRatio: 0.5,
     speed: 0.3
 });
+
+// Carousel handler
+$('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    infinite: true,
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1330,
+            settings: {
+                slidesToShow: 6,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 1120,
+            settings: {
+                slidesToShow: 5,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 970,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 740,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 570,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
