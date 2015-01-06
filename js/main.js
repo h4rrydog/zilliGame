c4c39a1aa384780d0d6d608f39df1404305bbc8f
h4rrydog <ifung@guanxin.de>
2015-01-06 14:04:48 +0000

    New prototype is working now.

--- js/main.js
+++ js/main.js
@@ -19,45 +19,11 @@ setInterval(swapHeaderBackground, 3000);
 // Carousel handler
 $('.carousel').slick({
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 5000,
     infinite: true,
-    slidesToShow: 7,
+    slidesToShow: 1,
     slidesToScroll: 1,
-    responsive: [
-        {
-            breakpoint: 1330,
-            settings: {
-                slidesToShow: 6,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 1120,
-            settings: {
-                slidesToShow: 5,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 970,
-            settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 740,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1
-            }
-        },
-        {
-            breakpoint: 570,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1
-            }
-        }
-    ]
+    arrows: false,
+    pauseOnHover: false,
+    fade: true
 });
